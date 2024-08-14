57eafd719f0d42b22d7cc0549de5e1645c3c57c3
Mariam Abdalmagied <mariamabdalmageid@gmail.com>
2024-08-14 18:27:06 +0300

    Update categoryController.js

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -3,11 +3,17 @@ const Category = require('../models/Category');
 // Create Category
 const createCategory = async(req, res) => {
     try {
-        const category = new Category(req.body);
-        await category.save();
-        res.status(201).send(category);
+        const { categoryName } = req.body;
+
+        if (!categoryName) {
+            return res.status(400).json({ error: 'categoryName is required' });
+        }
+
+        const newCategory = new Category({ categoryName });
+        await newCategory.save();
+        res.status(201).json(newCategory);
     } catch (err) {
-        res.status(400).send(err);
+        res.status(400).json({ error: err.message });
     }
 };
 
@@ -15,9 +21,9 @@ const createCategory = async(req, res) => {
 const getCategories = async(req, res) => {
     try {
         const categories = await Category.find();
-        res.send(categories);
+        res.json(categories);
     } catch (err) {
-        res.status(500).send(err);
+        res.status(500).json({ error: err.message });
     }
 };
 
@@ -25,10 +31,10 @@ const getCategories = async(req, res) => {
 const updateCategory = async(req, res) => {
     try {
         const category = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!category) return res.status(404).send();
-        res.send(category);
+        if (!category) return res.status(404).json({ error: 'Category not found' });
+        res.json(category);
     } catch (err) {
-        res.status(400).send(err);
+        res.status(400).json({ error: err.message });
     }
 };
 
@@ -36,10 +42,10 @@ const updateCategory = async(req, res) => {
 const deleteCategory = async(req, res) => {
     try {
         const category = await Category.findByIdAndDelete(req.params.id);
-        if (!category) return res.status(404).send();
-        res.send(category);
+        if (!category) return res.status(404).json({ error: 'Category not found' });
+        res.json(category);
     } catch (err) {
-        res.status(500).send(err);
+        res.status(500).json({ error: err.message });
     }
 };
 
