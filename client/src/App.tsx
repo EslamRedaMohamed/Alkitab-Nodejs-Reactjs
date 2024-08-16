bb28cbb7925f8363363120f6b1b730e8d5716009
Mariam Abdalmagied <mariamabdalmageid@gmail.com>
2024-08-16 08:14:12 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,39 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
 import AdminLayout from "./components/layout/admin/AdminLayout";
 import UserLayout from "./components/layout/user/UserLayout";
-
-// CRUD Pages
-import CategoriesPage from './pages/CategoriesPage';
-import AuthorsPage from './pages/AuthorsPage';
-import BooksPage from './pages/BooksPage';
 
 const App: React.FC = () => {
   // const { role } = useContext(AppContext);
   let role = "user";
   // let role = "admin";
-
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-[#F5F7F8]">
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
         {role === "admin" ? (
           <AdminLayout>
-            <Routes>
-              <Route path="/categories" element={<CategoriesPage />} />
-              <Route path="/authors" element={<AuthorsPage />} />
-              <Route path="/books" element={<BooksPage />} />
-            </Routes>
+            <AppRoutes isAdmin={true} />
           </AdminLayout>
         ) : (
           <UserLayout>
-            <Routes>
-              <Route path="/categories" element={<CategoriesPage />} />
-              <Route path="/authors" element={<AuthorsPage />} />
-              <Route path="/books" element={<BooksPage />} />
-            </Routes>
+            <AppRoutes isAdmin={false} />
           </UserLayout>
         )}
       </div>
-    </Router>
+    </BrowserRouter>
   );
 };
 
