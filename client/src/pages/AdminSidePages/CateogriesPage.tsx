d2c316848a0daae009d9b9fdd8e9daac4b21c124
Mariam Abdalmagied <mariamabdalmageid@gmail.com>
2024-08-20 02:38:36 +0300

    Update CateogriesPage.tsx

--- client/src/pages/AdminSidePages/CateogriesPage.tsx
+++ client/src/pages/AdminSidePages/CateogriesPage.tsx
@@ -1,6 +1,6 @@
+// client/src/pages/AdminSidePages/CategoriesPage.tsx
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import CategoryForm from '../../components/categories/CategoryForm';
 import CategoryList from '../../components/categories/CategoryList';
 
@@ -11,19 +11,7 @@ interface Category {
 
 const CategoriesPage: React.FC = () => {
   const [categories, setCategories] = useState<Category[]>([]);
-  const navigate = useNavigate(); // Initialize useNavigate
 
-  // Redirect to login if token doesn't exist
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      navigate('/admin/login'); // Redirect to login page
-    } else {
-      fetchCategories(); // Fetch categories if token exists
-    }
-  }, [navigate]); // Depend on navigate
-
-  // Fetch categories from the API
   const fetchCategories = async () => {
     try {
       const response = await axios.get<Category[]>('http://localhost:8080/categories/');
@@ -33,7 +21,6 @@ const CategoriesPage: React.FC = () => {
     }
   };
 
-  // Delete a category by ID
   const deleteCategory = async (id: string) => {
     try {
       await axios.delete(`http://localhost:8080/categories/${id}`);
@@ -43,16 +30,24 @@ const CategoriesPage: React.FC = () => {
     }
   };
 
-  // Conditional rendering to avoid rendering page content if redirected
-  if (!localStorage.getItem('token')) {
-    return null; // Do not render the page content
-  }
+  const updateCategory = async (id: string, newCategoryName: string) => {
+    try {
+      await axios.put(`http://localhost:8080/categories/${id}`, { categoryName: newCategoryName });
+      fetchCategories();
+    } catch (error) {
+      console.error("There was an error updating the category!", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl text-[#495E57] mb-4">Manage Categories</h1>
       <CategoryForm fetchCategories={fetchCategories} />
-      <CategoryList categories={categories} deleteCategory={deleteCategory} />
+      <CategoryList categories={categories} deleteCategory={deleteCategory} updateCategory={updateCategory} />
     </div>
   );
 };
