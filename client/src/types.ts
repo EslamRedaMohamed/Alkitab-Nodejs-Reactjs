15a7eadb29de0c6809b87c8d501ad4a0d94696e2
Mariam Abdalmagied <mariamabdalmageid@gmail.com>
2024-08-20 04:48:02 +0300

    Update types.ts

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,7 +1,21 @@
-// src/types.ts
 export interface Author {
+    photo: any;
     _id: string;
     fullName: string;
     dateOfBirth: string;
+  }
+
+
+  export interface Category {
+    _id: string;
+    categoryName: string;
+  }
+  
+  export interface Book {
+    _id: string;
+    name: string;
+    categoryName: string;
+    authorName: string;
+    photo?: string; // If book photo is available
   }
   
