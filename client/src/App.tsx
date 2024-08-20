b9fb55019b20ac73854f5a75ef3b6230d02e6d59
Mariam Abdalmagied <mariamabdalmageid@gmail.com>
2024-08-20 12:08:44 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import AdminLayout from "./components/layout/admin/AdminLayout";
 import UserLayout from "./components/layout/user/UserLayout";
+import React from 'react';
 
 const App: React.FC = () => {
-  // const { role } = useContext(AppContext);
-  // let role = "user";
-  let role = "admin";
+  const location = useLocation();
+  const isAdmin = location.pathname.startsWith('/admin');
+
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen">
-        {role === "admin" ? (
-          <AdminLayout>
-            <AppRoutes isAdmin={true} />
-          </AdminLayout>
-        ) : (
-          <UserLayout>
-            <AppRoutes isAdmin={false} />
-          </UserLayout>
-        )}
-      </div>
-    </BrowserRouter>
+    <div className="flex flex-col min-h-screen">
+      {isAdmin ? (
+        <AdminLayout>
+          <AppRoutes isAdmin={true} />
+        </AdminLayout>
+      ) : (
+        <UserLayout>
+          <AppRoutes isAdmin={false} />
+        </UserLayout>
+      )}
+    </div>
   );
 };
 
-export default App;
+const WrappedApp: React.FC = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+export default WrappedApp;
