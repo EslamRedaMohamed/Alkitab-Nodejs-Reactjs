64d06cc183a88d84a9b7451e8f94443099a9d055
Mariam Abdalmagied <mariamabdalmageid@gmail.com>
2024-08-21 22:43:45 +0300

    Update AuthorsPage.tsx

--- client/src/pages/AdminSidePages/AuthorsPage.tsx
+++ client/src/pages/AdminSidePages/AuthorsPage.tsx
@@ -3,14 +3,13 @@ import axios from 'axios';
 import { Author } from '../../types';
 import AuthorForm from '../../components/authors/AuthorForm';
 import AuthorList from '../../components/authors/AuthorList';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-
+import { useNavigate } from 'react-router-dom';
+//changed
 const AuthorsPage: React.FC = () => {
   const [authors, setAuthors] = useState<Author[]>([]);
   const [editingAuthor, setEditingAuthor] = useState<Author | null>(null);
-  const navigate = useNavigate(); // Correctly use `useNavigate` within the component
+  const navigate = useNavigate();
 
-  // Fetch authors from the API
   const fetchAuthors = async () => {
     try {
       const response = await axios.get<Author[]>('http://localhost:8080/authors/');
@@ -20,41 +19,38 @@ const AuthorsPage: React.FC = () => {
     }
   };
 
-  // Delete an author and refresh the list
   const deleteAuthor = async (id: string) => {
     try {
       await axios.delete(`http://localhost:8080/authors/${id}`);
-      fetchAuthors(); // Refresh the list after deleting an author
+      setAuthors((prevAuthors) => prevAuthors.filter((author) => author._id !== id));
     } catch (error) {
       console.error("There was an error deleting the author!", error);
     }
   };
 
-  // Set the author to be edited
   const updateAuthor = (author: Author) => {
-    setEditingAuthor(author); // Open the form with the author's data for editing
+    setEditingAuthor(author);
   };
 
-  // Redirect to login if token doesn't exist
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (!token) {
-      navigate('/admin/login'); // Redirect to login page if token doesn't exist
+      navigate('/admin/login');
     }
-  }, [navigate]); // Depend on navigate
+  }, [navigate]);
 
   useEffect(() => {
-    fetchAuthors(); // Fetch the authors on component mount
+    fetchAuthors();
   }, []);
-  // Conditional rendering to avoid rendering page content if redirected
+
   if (!localStorage.getItem('token')) {
-    return null; // Do not render the page content
+    return null;
   }
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl text-[#495E57] mb-4">Manage Authors</h1>
-      <AuthorForm fetchAuthors={fetchAuthors} editingAuthor={editingAuthor} setEditingAuthor={setEditingAuthor} />
+      <AuthorForm authors={authors} setAuthors={setAuthors} editingAuthor={editingAuthor} setEditingAuthor={setEditingAuthor} />
       <AuthorList authors={authors} deleteAuthor={deleteAuthor} updateAuthor={updateAuthor} />
     </div>
   );
