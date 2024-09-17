e1ec747474a875c5a44c1b264b55b44cfe4468e1
shellmage <kaddourafik@gmail.com>
2024-09-17 14:38:28 +0100

    Return the same error if username or password is incorrect (to prevent username enumeration), and sign jwt with secretkey

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,23 +25,22 @@ const verifyUser = async (req, res) => {
     const { username, password } = req.body;
     try {
         const user = await User.findOne({ username });
-        if (!user) return res.status(401).send('user name not found');
-
-        const decryptedPass = cryptoJs.AES.decrypt(user.password, secretKey);
-        if (password === decryptedPass.toString(cryptoJs.enc.Utf8)) {
-            return res.send({
-                id: user._id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                image: user.image,
-                username: user.username,
-                role: user.role,
-                token: jwt.sign({ _id: user._id, role: user.role }, 'key')
-            });
+        if (user) {
+            const decryptedPass = cryptoJs.AES.decrypt(user.password, secretKey);
+            if (password === decryptedPass.toString(cryptoJs.enc.Utf8)) {
+                return res.send({
+                    id: user._id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    image: user.image,
+                    username: user.username,
+                    role: user.role,
+                    token: jwt.sign({ _id: user._id, role: user.role }, secretKey)
+                });
+            }
         }
-
-        res.status(401).send('Un Authenticated');
+        return res.status(401).send('Wrong username or password');
     } catch (err) {
         console.log(err);
         res.status(400).send('Error verifying user');
